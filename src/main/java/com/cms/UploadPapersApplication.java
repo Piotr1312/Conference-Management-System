49fff4fa4311353e8f41c44d364aef9e2fb5fdfe
Piotr Kopka <piotrskopka@gmail.com>
2018-06-01 13:56:44 +0200

    Implement use-cases aasign reviewer and review paper

---
+++ src/main/java/com/cms/UploadPapersApplication.java
@@ -0,0 +1,21 @@
+package com.cms;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//Starting class
+@SpringBootApplication
+public class UploadPapersApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UploadPapersApplication.class);
+	}
+
+	public static void main(java.lang.String[] args) {
+		SpringApplication.run(UploadPapersApplication.class, args);
+	}
+	
+}
---
+++ src/main/java/com/cms/controller/HomeController.java
@@ -0,0 +1,94 @@
+package com.cms.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cms.entity.User;
+import com.cms.service.UserService;
+
+//handles all request for login, registration, news, schedule
+@Controller
+@ComponentScan(basePackages = {"com.wizzard.uploadpapers.service"})
+public class HomeController {
+
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/home", method = RequestMethod.GET)
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
+    }
+	
+	@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+	public ModelAndView login(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login");
+		return modelAndView;
+	}
+	
+	
+	@RequestMapping(value="/registration", method = RequestMethod.GET)
+	public ModelAndView registration(){
+		ModelAndView modelAndView = new ModelAndView();
+		User user = new User();
+		modelAndView.addObject("user", user);
+		modelAndView.setViewName("registration");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult, 
+			@RequestParam(value = "scientistReg", required = false) String checkboxValue) {
+		ModelAndView modelAndView = new ModelAndView();
+		User userExists;
+		
+		userExists = userService.findUserByEmail(user.getEmail());
+		if (userExists != null) {
+			bindingResult
+					.rejectValue("email", "error.user",
+							"There is already a user registered with the email provided");
+		}
+		if (bindingResult.hasErrors()) {
+			modelAndView.setViewName("registration");
+		} 
+		else {
+			if (checkboxValue != null)
+				userService.saveUser(user, "SCIENTIST");
+			else
+				userService.saveUser(user, "PARTICIPANT");
+			
+			modelAndView.addObject("successMessage", "User has been registered successfully");
+			modelAndView.addObject("scientist", new User());
+			modelAndView.setViewName("registration");
+			}
+		
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/news", method=RequestMethod.GET)
+    public ModelAndView showNews() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("news");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/schedule", method=RequestMethod.GET)
+    public ModelAndView showSchedule() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("schedule");
+		return modelAndView;
+	}
+	
+	
+}
